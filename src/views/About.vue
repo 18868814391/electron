74f116cd2b5548ed39e3672f84f5a36a55c3eff2
shenyifan <605729908@qq.com>
2021-09-06 09:57:35 +0800

    监听目录读取文件上传

--- src/views/About.vue
+++ src/views/About.vue
@@ -2,21 +2,35 @@
   <div class="about">
     <h1>This is an about page</h1>
     <button @click="selectPath()">选择监听目录</button>
-    <p>当前选择的目录为:{{path}}</p>
+    <div>
+      监听状态：{{listening}}
+    </div>
+    <p>当前选择的目录为:{{dicPath}}</p>
     <div class="hisBox">
-      <p v-for="(item,index) in history" :key="index">{{item}}</p>
+      <div v-for="(item,index) in history" :key="index">
+        {{item}}
+        <button v-if="index!==0" @click="goUpload(item)">手动上传</button>
+      </div>
+
     </div>
   </div>
 </template>
 <script>
+import axios from 'axios';
 const { ipcRenderer } = require('electron')
 // const watch = require('watch')
 const chokidar = require('chokidar');
+const fs = require('fs');
+// const mime = require('mime');
+// const request = require('request');
+const pathModal = require('path');
 export default {
   data() {
     return {
-      path: '',
+      dicPath: '',
+      listening: false,
       history: ['文件夹日志:']
+
     }
   },
   created() {
@@ -33,16 +47,17 @@ export default {
       ipcRenderer.on('selected-path', (event, pathList) => {
         console.log('backList', pathList)
         if (Array.isArray(pathList)) {
-          self.path = pathList[0]
-          self.startWatch(self.path)
+          self.dicPath = pathList[0]
+          self.startWatch(self.dicPath)
         }
       })
     },
-    startWatch(path) {
-      const watcher = chokidar.watch(path);
+    startWatch(dicPath) {
+      const self = this;
+      const watcher = chokidar.watch(dicPath);
       watcher
         .on('add', function(path) {
-          console.log('File', path, 'has been added');
+          self.getFile(path)
         })
         .on('addDir', function(path) {
           console.log('Directory', path, 'has been added');
@@ -63,7 +78,128 @@ export default {
     },
     onWatcherReady() {
       console.log('watch ready')
+      this.listening = true
+    },
+    getFile(file) {
+      console.log('监听到该文件add：', file)
+      if (this.history.indexOf(file) === -1) {
+        this.history.push(file)
+      }
+    },
+    goUpload(path) {
+      // const self = this
+      console.log(path)
+      const fileName = pathModal.basename(path);
+      console.log('fileName', fileName)
+      fs.readFile(path, function(err, data) {
+        if (err) {
+          console.log('读取错误', err)
+        } else {
+          console.log('bufferData', data)
+        }
+        var fff = new File([data], fileName, {type: 'text/plain'})
+        // fff.path = path
+        // console.log('ffff', fff)
+        // console.log('binary', fff)
+        const param = new FormData(); // 创建form对象
+        param.append('file', fff); // 通过append向form对象添加数据
+        param.append('isOriginalFilename', false); // 添加form表单中其他数据
+        param.append('applicationMark', 'health'); // 添加form表单中其他数据
+        axios.post('https://api.qdsgvision.com/upload/upload/single',
+          param,
+          {
+            headers: {
+              'Content-Type': 'multipart/form-data'
+            }
+          }
+        )
+        // const reader = new FileReader();
+        // reader.readAsBinaryString(fff);
+        // reader.onloadend = (e) => {
+        //   console.log('binary', e)
+        //   const param = new FormData(); // 创建form对象
+        //   param.append('file', e); // 通过append向form对象添加数据
+        //   param.append('isOriginalFilename', false); // 添加form表单中其他数据
+        //   param.append('applicationMark', 'health'); // 添加form表单中其他数据
+        //   axios.post('https://apitest.qdsgvision.com/upload/upload/single',
+        //     param,
+        //     {
+        //       headers: {
+        //         'Content-Type': 'multipart/form-data'
+        //       }
+        //     }
+        //   )
+        //   // axios({
+        //   //   url: 'https://apitest.qdsgvision.com/upload/upload/single',
+        //   //   method: 'post',
+        //   //   headers: {
+        //   //     'Content-Type': 'multipart/form-data'
+        //   //   },
+        //   //   data: {
+        //   //     file: e,
+        //   //     isOriginalFilename: false,
+        //   //     applicationMark: 'health'
+        //   //   }
+        //   // })
+        // }
+      })
+      // const buffer = fs.createReadStream(path)
+      // console.log('流', buffer)
+      // const fff = new File([buffer._readableState.buffer.tail.data], fileName, {type: 'text/plain'})
+      // console.log('ffff', fff)
+      // axios({
+      //   url: 'https://apitest.qdsgvision.com/upload/upload/single',
+      //   method: 'post',
+      //   headers: {
+      //     'Content-Type': 'multipart/form-data'
+      //   },
+      //   data: {
+      //     file: buffer,
+      //     isOriginalFilename: true
+      //   }
+      // })
+
+      // request.post({
+      //   url: 'https://apitest.qdsgvision.com/upload/upload/single',
+      //   formData: {
+      //     file: buffer,
+      //     isOriginalFilename: true
+      //   }
+      // },
+      // function optionalCallback(err, httpResponse, body) {
+      //   if (err) {
+      //     return console.error('upload failed:', err);
+      //   }
+      // }
+      // )
+      // fs.stat(path, (err, stats) => {
+      //   if (err) throw err;
+      //   console.log(stats)
+      // })
     }
+    // bytesToBinary(bytes) {
+    //   const length = bytes.length;
+    //   let result = '';
+    //   for (let i = 0; i < length; i++) {
+    //     const binStr = Number(bytes[i]).toString(16)
+    //     result += '0'.repeat(2 - binStr.length) + binStr; // 不足二位前置补0
+    //   }
+    //   return result.toString();
+    // },
+    // 读取文件信息
+    // getFileStats(path, callback) {
+    //   fs.stat(path, (err, status) => {
+    //     if (err) {
+    //       callback(err);
+    //     } else {
+    //       const index = path.lastIndexOf('\\');
+    //       const len = path.length;
+    //       const filename = index !== -1 ? path.substring(index + 1, len) : path;
+    //       const mimetype = mime.lookup(path)
+    //       callback(null, {size: status.size, name: filename, path: path, type: mimetype});
+    //     }
+    //   })
+    // }
   }
 
 }
